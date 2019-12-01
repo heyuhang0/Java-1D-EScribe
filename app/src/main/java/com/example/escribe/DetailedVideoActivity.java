34a225043bd0837970ce99a3f3c259c6446c7763
heyuhang0 <heyuhang0@outlook.com>
2019-12-01 17:58:17 +0800

    feat: UI design for comments

--- app/src/main/java/com/example/escribe/DetailedVideoActivity.java
+++ app/src/main/java/com/example/escribe/DetailedVideoActivity.java
@@ -2,18 +2,20 @@ package com.example.escribe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -22,6 +24,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class DetailedVideoActivity extends AppCompatActivity {
@@ -32,6 +36,8 @@ public class DetailedVideoActivity extends AppCompatActivity {
     private static final String STATE_VIDEO_POSITION = "video_position";
     private boolean videoPlaying = false;
     private int videoPosition = 1;
+
+    private RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +96,27 @@ public class DetailedVideoActivity extends AppCompatActivity {
             video.setMediaController(mc);
             mc.setAnchorView(video);
         }));
+
+        List<String> comments = new ArrayList<>();
+        comments.add("Good video");
+        comments.add("Nice app");
+
+        RecyclerView recyclerView = findViewById(R.id.comments_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new CommentsAdapter(comments);
+        recyclerView.setAdapter(mAdapter);
+
+        Button postCommentButton = findViewById(R.id.send_comment);
+        EditText editCommentText = findViewById(R.id.edit_comment);
+
+        postCommentButton.setOnClickListener(v -> {
+            String comment = editCommentText.getText().toString();
+            comments.add(comment);
+            editCommentText.setText("");
+            mAdapter.notifyItemInserted(comments.size() - 1);
+            recyclerView.smoothScrollToPosition(mAdapter.getItemCount() - 1);
+        });
     }
 
     @Override
@@ -104,5 +131,42 @@ public class DetailedVideoActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putBoolean(STATE_VIDEO_PLAYING, videoPlaying);
         savedInstanceState.putInt(STATE_VIDEO_POSITION, videoPosition);
+    }
+
+    class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.MyViewHolder> {
+
+        private List<String> comments;
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            MyViewHolder(View v) {
+                super(v);
+                textView = v.findViewById(R.id.comment_item_text);
+            }
+        }
+
+        CommentsAdapter(List<String> comments) {
+            this.comments = comments;
+        }
+
+
+        @NonNull
+        @Override
+        public CommentsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.comment_item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CommentsAdapter.MyViewHolder holder, int position) {
+            holder.textView.setText(comments.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
     }
 }
