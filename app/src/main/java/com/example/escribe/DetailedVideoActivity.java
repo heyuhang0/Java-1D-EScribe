fd944911bde783716473729f58ef29b8a2b664c6
heyuhang0 <heyuhang0@outlook.com>
2019-12-01 16:34:16 +0800

    feat: full screen video player when rotate

--- app/src/main/java/com/example/escribe/DetailedVideoActivity.java
+++ app/src/main/java/com/example/escribe/DetailedVideoActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,12 +22,30 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Objects;
+
 public class DetailedVideoActivity extends AppCompatActivity {
+
+    private VideoView video;
+
+    private static final String STATE_VIDEO_PLAYING = "video_playing";
+    private static final String STATE_VIDEO_POSITION = "video_position";
+    private boolean videoPlaying = false;
+    private int videoPosition = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_video);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
+        else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            Objects.requireNonNull(getSupportActionBar()).show();
+        }
 
         Intent intent = getIntent();
         String courseName = intent.getStringExtra(Util.MESSAGE_COURSE_NAME);
@@ -32,7 +54,7 @@ public class DetailedVideoActivity extends AppCompatActivity {
         setTitle(lessonName);
         String slidePath = courseName + "/" + lessonName + "/processedVideos/" + slideIndex;
 
-        VideoView video = findViewById(R.id.video);
+        video = findViewById(R.id.video);
         TextView transcriptView = findViewById(R.id.transcript);
         TextView slideRecognitionView = findViewById(R.id.slide_recognition);
 
@@ -46,7 +68,15 @@ public class DetailedVideoActivity extends AppCompatActivity {
                 slideRecognitionView.setText(slide.getSlidesRecognition());
 
                 video.setVideoURI(Uri.parse(slide.getUrl()));
-                video.seekTo(1);
+
+                if (savedInstanceState != null) {
+                    video.seekTo(savedInstanceState.getInt(STATE_VIDEO_POSITION));
+                    if (savedInstanceState.getBoolean(STATE_VIDEO_PLAYING)) {
+                        video.start();
+                    }
+                } else {
+                    video.seekTo(1);
+                }
             }
 
             @Override
@@ -60,5 +90,19 @@ public class DetailedVideoActivity extends AppCompatActivity {
             video.setMediaController(mc);
             mc.setAnchorView(video);
         }));
+    }
+
+    @Override
+    protected void onPause() {
+        videoPosition = video.getCurrentPosition();
+        videoPlaying = video.isPlaying();
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(STATE_VIDEO_PLAYING, videoPlaying);
+        savedInstanceState.putInt(STATE_VIDEO_POSITION, videoPosition);
     }
 }
